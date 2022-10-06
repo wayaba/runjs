568bb702ef99fc8c8ca1ddae59fd404c8f71f278
Pablo Pedraza <wensel84@gmail.com>
2022-10-06 09:49:14 -0300

    fix ej 25

--- 25-midurat.js
+++ 25-midurat.js
@@ -23,7 +23,50 @@ canMouseEat("down", room2); // false
 canMouseEat("right", room2); // true
 canMouseEat("left", room2); // false
 
-export default function canMouseEat(direction: String, game: Array) {
+
+const room3 = [["*", "m"]];
+
+canMouseEat("down", room3); // false
+
+
+const tests = [
+  { direction: 'up', game: [[" ", " ", " "], [" ", " ", "m"], [" ", " ", "*"]], expected: false },
+  
+  { direction: 'down', game: [[" ", " ", " "], [" ", " ", "m"], [" ", " ", "*"]], expected: true },
+  
+  { direction: 'right', game: [[" ", " ", " "], [" ", " ", "m"], [" ", " ", "*"]], expected: false },
+  { direction: 'left', game: [[" ", " ", " "], [" ", " ", "m"], [" ", " ", "*"]], expected: false },
+
+  { direction: 'up', game: [["*", " ", " ", " "], [" ", "m", "*", " "], [" ", " ", " ", " "], [" ", " ", " ", "*"]], expected: false },
+  { direction: 'down', game: [["*", " ", " ", " "], [" ", "m", "*", " "], [" ", " ", " ", " "], [" ", " ", " ", "*"]], expected: false },
+  { direction: 'right', game: [["*", " ", " ", " "], [" ", "m", "*", " "], [" ", " ", " ", " "], [" ", " ", " ", "*"]], expected: true },
+  { direction: 'left', game: [["*", " ", " ", " "], [" ", "m", "*", " "], [" ", " ", " ", " "], [" ", " ", " ", "*"]], expected: false },
+
+  { direction: 'up', game: [["*", "m"]], expected: false },
+  { direction: 'down', game: [["*", "m"]], expected: false },
+  { direction: 'right', game: [["*", "m"]], expected: false },
+  { direction: 'left', game: [["*", "m"]], expected: true },
+
+  { direction: 'up', game: [["*", "*", "*"], ["*", "m", "*"], ["*", "*", "*"]], expected: true },
+  { direction: 'down', game: [["*", "*", "*"], ["*", "m", "*"], ["*", "*", "*"]], expected: true },
+  { direction: 'right', game: [["*", "*", "*"], ["*", "m", "*"], ["*", "*", "*"]], expected: true },
+  { direction: 'left', game: [["*", "*", "*"], ["*", "m", "*"], ["*", "*", "*"]], expected: true },
+
+  { direction: 'up', game: [[" ", " ", " "], [" ", "m", " "], [" ", " ", " "]], expected: false },
+  { direction: 'down', game: [[" ", " ", " "], [" ", "m", " "], [" ", " ", " "]], expected: false },
+  { direction: 'right', game: [[" ", " ", " "], [" ", "m", " "], [" ", " ", " "]], expected: false },
+  { direction: 'left', game: [[" ", " ", " "], [" ", "m", " "], [" ", " ", " "]], expected: false },
+
+];
+
+tests.forEach(e => {
+  const result = canMouseEat(e.direction, e.game)
+  console.log('e.treeA :>> ', e.direction, '\n> e.treeB :', e.game)
+  console.log('> Expected:', e.expected, '\n> Result:', result)
+  console.log(`Test Passed: ${(e.expected === result) ? 'True :)' : 'False !!!'}`, '\n ')
+});
+
+export default function canMouseEat(direction, game) {
   // ¡Gracias por jugar a AdventJS 2021! 🤗
   // ¡Nos vemos el año que viene! 👋
   // Por favor, comparte en las redes qué te ha parecido! 🚀
@@ -40,9 +83,7 @@ export default function canMouseEat(direction: String, game: Array) {
     right: { row: indexX, col: indexY + 1 },
   };
 
-  return (
-    game[directionMap[direction].row][directionMap[direction].col] !=
-      undefined &&
-    game[directionMap[direction].row][directionMap[direction].col] == "*"
-  );
+  if (game[directionMap[direction].row] === undefined || game[directionMap[direction].col] === undefined)
+    return false;
+  return game[directionMap[direction].row][directionMap[direction].col] == "*";
 }
