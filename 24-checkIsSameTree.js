734d6e97b9760322616e6769b88d53d0a6537453
Pablo Pedraza <wensel84@gmail.com>
2022-11-08 09:01:14 -0300

    fix ej 24

--- 24-checkIsSameTree.js
+++ 24-checkIsSameTree.js
@@ -1,5 +1,5 @@
 //https://adventjs.dev/challenges/24
-/*
+
 const tree = {
   value: null,
   left: { value: 2, left: null, right: null },
@@ -30,80 +30,34 @@ checkIsSameTree(tree2, tree2) // true
     }
  
 checkIsSameTree(tree3, tree4) // true
-*/
-
- 
-
-const tests = [
-  {
-    treeA: {
-      value: 1,
-      left: { value: 2, left: null, right: null },
-      right: { value: 3, left: null, right: null }
-    }, treeB: {
-      value: 1,
-      left: { value: 2, left: null, right: null },
-      right: { value: 3, left: null, right: null }
-    }, expected: true
-  },
-  {
-    treeA: {
-      value: 1,
-      left: { value: 3, left: { value: 2, left: null, right: null }, right: null },
-      right: { value: 5, left: null, right: { value: 4, left: null, right: null } }
-    }, treeB: {
-      value: 1,
-      left: { value: 3, left: { value: 2, left: null, right: null }, right: null },
-      right: { value: 5, left: null, right: { value: 4, left: null, right: null } }
-    }, expected: true
-  },
-  {
-    treeA: {
-      value: 1,
-      left: { value: 3, left: { value: 2, left: null, right: null }, right: null },
-      right: { value: 5, left: null, right: { value: 4, left: null, right: null } }
-    }, treeB: {
-      value: 1,
-      left: { value: 3, left: { value: 2, left: null, right: null }, right: null },
-      right: { value: 5, left: null, right: { value: 4, left: null, right: null } }
-    }, expected: false
-  }
-]
-tests.forEach(e => {
-  const result = checkIsSameTree(e.treeA, e.treeB)
-  console.log('e.treeA :>> ', e.treeA, '\n> e.treeB :', e.treeB)
-  console.log('> Expected:', e.expected, '\n> Result:', result)
-  console.log(`Test Passed: ${(e.expected === result) ? 'True :)' : 'False !!!'}`, '\n ')
-});
 
 
-const tree11= {
-      value: 1,
-      left: { value: 2, left: null, right: null },
-      right: { value: 3, left: null, right: null }
-    }
- const tree22 = {
-      value: 1,
-      left: { value: 2, left: null, right: null },
-      right: { value: 3, left: null, right: null }
- }
- checkIsSameTree(tree11, tree11) // true
+const tree11 = {
+  value: 1,
+  left: { value: 2, left: null, right: null },
+  right: { value: 3, left: null, right: null },
+};
+const tree22 = {
+  value: 1,
+  left: { value: 2, left: null, right: null },
+  right: { value: 3, left: null, right: null },
+};
+checkIsSameTree(tree11, tree11); // true
 
 export default function checkIsSameTree(treeA, treeB) {
- // ¡No olvides compartir tu solución en redes!
-  
-  const validate = (a, b) =>{
-    return Object.keys(a).every(key=>{
-      if(a[key] === null || a[key] === undefined) return true
-      if(typeof a[key] === 'object') 
-        validate(a[key],b[key]) 
-      else{
-        if(a[key] !== b[key]) return false
-      }
-      
-      return true
-    })
-  }
-  
-  return validate(treeA,treeB);
+  // ¡No olvides compartir tu solución en redes!
+
+  const validate = (a, b) => {
+    if (typeof a !== typeof b) return false;
+    if (a !== null && typeof a === "object") {
+      let value = validate(a.value, b.value);
+      let left = validate(a.left, b.left);
+      let right = validate(a.right, b.right);
+      return value && left && right;
+    } else {
+      return a === b;
+    }
+  };
+
+  return validate(treeA, treeB);
 }
